6ae50e4c4936eaafd49350fdc163d150f6f9dae4
Vincent Payet <vpayet@isara.fr>
2023-02-08 15:21:19 +0100

    signe reactive et graphique

--- astro.R
+++ astro.R
@@ -1,54 +1,71 @@
-<<<<<<< HEAD
 library(shiny)
+library(plotly)
 
 ui <- fluidPage(
   titlePanel("Déterminer son signe astrologique"),
   sidebarLayout(
     sidebarPanel(
-      dateInput("date", "Entrez votre date de naissance :", value = Sys.Date(), format = "dd/mm/yy"),
+      dateInput("date", "Entrez votre date de naissance :", value = Sys.Date(), format = "dd/mm/yyyy"),
       submitButton("Submit")
     ),
     mainPanel(
-      textOutput("signe")
+      textOutput("signe"),
+      plotlyOutput("pie")
     )
   )
 )
 
 server <- function(input, output) {
-  output$signe <- renderText({
+  
+  signe <- reactive({
     jour <- as.integer(format(input$date, "%d"))
     mois <- as.integer(format(input$date, "%m"))
-    signe <- ""
     if ((mois == 3 & jour >= 21) || (mois == 4 & jour <= 19)) {
-      signe <- "Bélier"
+      "Bélier"
     } else if ((mois == 4 & jour >= 20) || (mois == 5 & jour <= 20)) {
-      signe <- "Taureau"
+      "Taureau"
     } else if ((mois == 5 & jour >= 21) || (mois == 6 & jour <= 20)) {
-      signe <- "Gémeaux"
+      "Gémeaux"
     } else if ((mois == 6 & jour >= 21) || (mois == 7 & jour <= 22)) {
-      signe <- "Cancer"
+      "Cancer"
     } else if ((mois == 7 & jour >= 23) || (mois == 8 & jour <= 22)) {
-      signe <- "Lion"
+      "Lion"
     } else if ((mois == 8 & jour >= 23) || (mois == 9 & jour <= 22)) {
-      signe <- "Vierge"
+      "Vierge"
     } else if ((mois == 9 & jour >= 23) || (mois == 10 & jour <= 22)) {
-      signe <- "Balance"
+      "Balance"
     } else if ((mois == 10 & jour >= 23) || (mois == 11 & jour <= 21)) {
-      signe <- "Scorpion"
+      "Scorpion"
     } else if ((mois == 11 & jour >= 22) || (mois == 12 & jour <= 21)) {
-      signe <- "Sagittaire"
+      "Sagittaire"
     } else if ((mois == 12 & jour >= 22) || (mois == 1 & jour <= 19)) {
-      signe <- "Capricorne"
+      "Capricorne"
     } else if ((mois == 1 & jour >= 20) || (mois == 2 & jour <= 18)) {
-      signe <- "Verseau"
+      "Verseau"
     } else if ((mois == 2 & jour >= 19) || (mois == 3 & jour <= 20)) {
-      signe <- "Poissons"
+      "Poissons"
     }
-    paste("Votre signe astrologique est :", signe)
   })
+  
+  
+  output$signe <- renderText({
+    paste("Votre signe astrologique est :", signe())
+  })
+  
+  output$pie <- renderPlotly({
+    
+    cara <- read.csv2("caracteristiques.csv", sep=";")
+    
+    x <- cara[cara$signe==signe(), ]
+    si <- unlist(x[2:3])
+    
+    # Graphique
+    camembert <- plot_ly(labels = names(si), 
+                         values = si, type = "pie")
+    
+  })
+  
+  
 }
 
 shinyApp(ui, server)
-=======
-fizebifbzefbzifbofbzogbgozrgn
->>>>>>> origin/main
