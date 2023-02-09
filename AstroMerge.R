04d9f5222156055130d9eab3bf1bb79903d76e2e
clairetertrais <ctertrais@etu.isara.fr>
2023-02-09 15:17:53 +0100

    Version avec tout qui fonctionne

--- AstroMerge.R
+++ AstroMerge.R
@@ -15,6 +15,8 @@ ui <- fluidPage(
       textOutput("description"),
       h2("Signe Chinois :"),
       textOutput("signe_chinois"),
+      h2("Voici ta compatibilité amoureuse :"),
+      textOutput("compatibilite"),
       h3 ("Voici tes traits de caractères principaux :"),
       plotlyOutput("pie")
     )
@@ -123,7 +125,39 @@ server <- function(input, output) {
     }
     paste("Ton signe chinois est :", signe_chinois)
   })
+
+  output$compatibilite <- renderText({
+    compatibilite <- ""
+    if (signe()=="Bélier") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Sagittaire,vous connaitrez la passion. Avec le Taureau vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Lion, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Taureau") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Capricorne,vous connaitrez la passion. Avec le Scorpion vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Bélier, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Gémeaux") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Gémeaux,vous connaitrez la passion. Avec la Vierge vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Sagittaire, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Cancer") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Lion,vous connaitrez la passion. Avec la Vierge vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Capricorne, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Lion") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Sagittaire,vous connaitrez la passion. Avec la Balance vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Bélier, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Vierge") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Capricorne,vous connaitrez la passion. Avec le Taureau vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Cancer, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Balance") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Lion,vous connaitrez la passion. Avec le Taureau vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Bélier, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Scorpion") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Cancer,vous connaitrez la passion. Avec le Taureau vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Capricorne, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Sagittaire") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Lion,vous connaitrez la passion. Avec le Bélier vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Gémeaux, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Capricorne") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Capricorne,vous connaitrez la passion. Avec le Taureau vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Scorpion, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Verseau") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Sagittaire,vous connaitrez la passion. Avec le Verseau vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec le Scorpion, qui ne tente rien n'a rien ! "
+    } else if (signe()=="Poissons") {
+      compatibilite <- "Votre meilleure compatibilité amoureuse est avec le Scorpion,vous connaitrez la passion. Avec le Cancer vous avez une bonne compatibilité, à exploiter. Enfin, ça peut toujours marcher avec la Vierge, qui ne tente rien n'a rien ! "
+    }
+    paste("",compatibilite)
+  })  
   
+  
+    
   
   output$pie <- renderPlotly({
     
