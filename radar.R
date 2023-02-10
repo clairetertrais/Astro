d97a72db8260e4b7bb86be12b01d0d8c47e6f023
alactalbumine <ebarthelemy@etu.isara.fr>
2023-02-10 09:13:58 +0100

    radar marche

--- radar.R
+++ radar.R
@@ -1,21 +1,35 @@
-#install.packages("fmsb") # permet d'installer le package
 library(fmsb)
 
 cara <- read.csv2("Tableau_qualités_signes.csv", sep=";")
 
-x <- cara[cara$signe=="Poissons", ] #pour débug on donne une valeur à signe
-
-#si <- unlist(x)
-
-cara <- cara[,-c(1)] #essai pour enlever le signe dans le graph qui apparait comme une caractéristique
-#cara <- as.data.frame(cara)
-cara
-cara <- cara[cara != 0]
-cara <- data.frame(cara)
-  #si <- si[si != 0]
-#si <- data.frame(si)
+# Demander à l'utilisateur de choisir un signe astrologique
+signe <- "Cancer"
 
 
-radarchart(cara,  axistype = 2, cglty = 3, title = "Diagramme en radar") #problème car les valeurs ne sont pas considérées comme numériques. 
-#Essai de faire passer en data.frame mais pas concluant
-#Tentative échouée 
+# Sélectionner les données pour le signe choisi
+x <- cara[cara$signe == signe, ]
+
+# Supprimer les colonnes qui ont une valeur égale à 0
+x <- x[, colSums(x == 0) == 0]
+
+# Enlever la première colonne (signe)
+x <- x[, -1]
+
+x <- rbind(rep(45,27) , rep(0,27) , x)
+
+# Transformer les données en data.frame
+x <- data.frame(x)
+
+
+# Générer le diagramme en radar
+radarchart( x  , axistype=1 , 
+            
+            #custom polygon
+            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
+            
+            #custom the grid
+            cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8,
+            
+            #custom labels
+            vlcex=0.8 
+)
